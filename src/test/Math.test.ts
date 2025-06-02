a1ea6b416fb3a7409508e5f26516932660942213
camilazci <camilazucchi@outlook.com>
2025-06-02 13:12:23 -0300

    test(exercise): add unit test for sum function

---
+++ src/test/Math.test.ts
@@ -0,0 +1,17 @@
+import { sum } from "../app/Math";
+
+describe("Calculator", () => {
+    it("Should return the sum of two numbers", () => {
+        // Arrange
+        const sut = sum;
+        const num1 = 5;
+        const num2 = 3;
+        const expected = 8;
+
+        // Act
+        const actual = sut(num1, num2);
+
+        // Assert
+        expect(actual).toBe(expected);
+    })
+});
