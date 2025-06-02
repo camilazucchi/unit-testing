4862e0439a7d7b0296bc987cdba10ee43249675a
camilazci <camilazucchi@outlook.com>
2025-06-02 12:15:13 -0300

    refactor(test): apply AAA pattern toUpperCase test

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -3,17 +3,24 @@ import { toUpperCase } from "../app/Utils";
 
 /* 'describe' é uma função do Jest que serve para AGRUPAR TESTES relacionados. É como criar uma "seção" ou "capítulo".
 'Utils test suite' é o nome do grupo de testes. */
-describe('Utils test suite', () => {
+describe("Utils test suite", () => {
+	/* 'test' (ou 'it') é a função principal do Jest para definir um teste individual.
+	'Should return...' é uma descrição clara e legível do que este teste específico deve verificar.
+	'() => {...}' é a função onde o teste acontece. */
+	it("Should return UpperCase of a valid String", () => {
+		/* Arrange
+		'sut' é uma abreviação para "System Under Test". É uma CONVENÇÃO COMUM em testes de unidade. Ela se refere à
+		peça específica do código que estamos testando nesse momento. */
+		const sut = toUpperCase;
+		const expected = "TEST";
 
-    /* 'test' (ou 'it') é a função principal do Jest para definir um teste individual.
-    'Should return...' é uma descrição clara e legível do que este teste específico deve verificar.
-    '() => {...}' é a função onde o teste acontece. */
-    test('Should return UpperCase', () => {
-        /* A função 'toUpperCase' é executada com a palavra 'test' como entrada. */
-        const result = toUpperCase('test');
-        /* Esta é a asserção (ou verificação) do teste. É onde comparamos o que o nosso código realmente fez
-        com o que esperávamos que ele fizesse. */
-        expect(result).toBe('Test');
-    });
+		/* Act
+		A função 'toUpperCase' é executada com a palavra 'test' como entrada. */
+		const actual = sut("test");
 
+		/* Assert
+		Esta é a asserção (ou verificação) do teste. É onde comparamos o que o nosso código realmente fez
+		com o que esperávamos que ele fizesse. */
+		expect(actual).toBe(expected);
+	});
 });
