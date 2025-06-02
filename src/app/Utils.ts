78b2844bbe2f4a707151068d9a9de2cc293aa662
camilazci <camilazucchi@outlook.com>
2025-06-02 14:36:14 -0300

    feat(string-utils): add getStringInfo function

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(arg: string) {
     return arg.toUpperCase();
 }
+
+export type stringInfo = {
+    lowerCase: string,
+    upperCase: string,
+    characters: string[],
+    length: number,
+    extraInfo: Object | undefined // Last one can have or omit, but comma is typically preferred.
+};
+
+export function getStringInfo(arg: string): stringInfo {
+    return {
+        lowerCase: arg.toLowerCase(),
+        upperCase: arg.toUpperCase(),
+        characters: Array.from(arg),
+        length: arg.length, // 'length' is a property, not a method, that's why it does not have parentheses.
+        extraInfo: {}
+    }
+}
