499ce26531c7e04ffaf4e4f4aeb5e33a4f9bfca5
camilazci <camilazucchi@outlook.com>
2025-06-04 10:50:58 -0300

    feat(SecurePassTS): implement basic password lowercase and uppercase checks

--- src/app/secure_pass/SecurePassTS.ts
+++ src/app/secure_pass/SecurePassTS.ts
@@ -1,5 +1,18 @@
 export class SecurePass {
     public checkPassword(password: string): boolean {
-        return password.length >= 8 ? true : false;
+        if (password.length < 8) {
+            return false;
+        }
+
+        // Se a string password for igual à string password com lowercase, significa que nossa senha não tem pelo menos um caracter em upper case.
+        if (password == password.toLowerCase()) {
+            return false;
+        }
+
+        if (password == password.toUpperCase()) {
+            return false;
+        }
+
+        return true;
     }
 }
