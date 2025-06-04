298903d3ab985562bff9613911173d56b70670b2
camilazci <camilazucchi@outlook.com>
2025-06-04 10:50:20 -0300

    test(SecurePassTS): add basic password upper case and lower case validation tests

--- src/test/secure_pass/SecurePassTS.test.ts
+++ src/test/secure_pass/SecurePassTS.test.ts
@@ -17,7 +17,31 @@ describe("Secure Pass Test Suite", () => {
 
     // Test 2: Password with more or equal than 8 characters 
     it("Password with more or equal than 8 characters is valid", () => {
-        const actual = sut.checkPassword("12345678");
+        const actual = sut.checkPassword("1234567bA");
+        expect(actual).toBe(true);
+    });
+
+    // Test 3: Password with no upper case letter
+    it("Password with no upper case letter is invalid", () => {
+        const actual = sut.checkPassword("1234abcd");
+        expect(actual).toBe(false);
+    });
+
+    // Test 4: Password with upper case letter
+    it("Password with upper case letter is valid", () => {
+        const actual = sut.checkPassword("1234Abcd");
+        expect(actual).toBe(true);
+    });
+
+    // Test 5: Password with no lower case letter
+    it("Password with no lower case letter is invalid", () => {
+        const actual = sut.checkPassword("123456ABCD");
+        expect(actual).toBe(false);
+    });
+
+    // Test 6: Password with lower case letter
+    it("Password with lower case letter is valid", () => {
+        const actual = sut.checkPassword("123456AbCD");
         expect(actual).toBe(true);
     });
 });
