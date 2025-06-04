03c04730da33ffe49064f5847b1626024ebfb3f8
camilazci <camilazucchi@outlook.com>
2025-06-04 12:24:00 -0300

    feat(SecurePassTS): implement enum, interface and refactor validations to SecurePassTS class

--- src/app/secure_pass/SecurePassTS.ts
+++ src/app/secure_pass/SecurePassTS.ts
@@ -1,18 +1,46 @@
+// Criamos um ENUM para definir as mensagens de erro:
+export enum PasswordErrors {
+    SHORT = "Password is too short.",
+    NO_UPPER_CASE = "Upper case letter required.",
+    NO_LOWER_CASE = "Lower case letter required."
+}
+
+// Define a estrutura do objeto que será usado na validação de senha:
+export interface CheckResult {
+    valid: boolean,
+    reasons: PasswordErrors[]
+}
+
+// Classe principal com a lógica de validação de senha:
 export class SecurePass {
-    public checkPassword(password: string): boolean {
+
+    public checkPassword(password: string): CheckResult {
+
+        // Criamos uma lista vazia chamada reasons, ela será preenchida com os erros encontrados na senha:
+        const reasons: PasswordErrors[] = [];
+
+        // Verifica se o comprimento da senha é menor do que 8 chars:
         if (password.length < 8) {
-            return false;
+            reasons.push(PasswordErrors.SHORT);
         }
 
-        // Se a string password for igual à string password com lowercase, significa que nossa senha não tem pelo menos um caracter em upper case.
+        // Compara a senha original com a mesma senha convertida para todas as letras minúsculas, se as duas forem iguais, significa que a senha não continha nenhuma letra maiúscula:
         if (password == password.toLowerCase()) {
-            return false;
+            reasons.push(PasswordErrors.NO_UPPER_CASE);
         }
 
+        // Mesma coisa da comparação acima, trocando somente a conversão para todas as letras maísculas, se as duas forem iguais, a senha não continha nenhuma letra minúscula.
         if (password == password.toUpperCase()) {
-            return false;
+            reasons.push(PasswordErrors.NO_LOWER_CASE);
         }
 
-        return true;
+        // O método retorna um objeto do tipo CheckResult:
+        return {
+            // Verifica se a lista reasons tem algum item, se tiver mais de 0 itens, significa que houve erros, então valid será false.
+            // Se tiver 0 itens, significa que não houve erros, então valid será true.
+            valid: reasons.length > 0 ? false : true,
+            reasons: reasons
+        }
     }
+
 }
